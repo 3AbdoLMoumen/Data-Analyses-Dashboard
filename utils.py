2c9e23eff6138bf5bf6efe228eadfec67b1befa3
MECHEGUEG Iyad AbdeL'Moumen <mecheguegiyadabdelmoumen@gmail.com>
2026-01-29 22:05:12 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -17,7 +17,22 @@ def numerical2classes(col):
     bins.append(col.max() + 1e-8)
     labels = [f"{int(bins[i])}-{int(bins[i+1])}" for i in range(numC)]
     return pd.cut(col, bins=bins, labels=labels, include_lowest=True)
-
 def Frequency(col):
+	freq = col.value_counts()
+	return freq
 
-    return col.value_counts().reset_index().rename(columns={"index": col, col: "Frequency"})
+def StatisticalParams(df, numerical_cols):
+	return pd.DataFrame({
+    "count": df[numerical_cols].count(),
+    "mean": df[numerical_cols].mean(),
+    "median": df[numerical_cols].median(),
+    "mode": df[numerical_cols].apply(lambda x: x.mode().iloc[0] if not x.mode().empty else np.nan),
+    "min": df[numerical_cols].min(),
+    "max": df[numerical_cols].max(),
+    "range": df[numerical_cols].max() - df[numerical_cols].min(),
+    "variance": df[numerical_cols].var(),
+    "std_dev": df[numerical_cols].std(),
+    "Q1": df[numerical_cols].quantile(0.25),
+    "Q3": df[numerical_cols].quantile(0.75),
+    "IQR": df[numerical_cols].quantile(0.75) - df[numerical_cols].quantile(0.25)
+})
